de264a1d392751d199ab4f35b9b3db03a1706989
uekinokotton <cstk23020@g.nihon-u.ac.jp>
2024-12-31 18:29:23 +0900

    よくわからん

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N, A[201];
-
 int main(){
-    cin >> N;
-    for(int i = 0; i<N; i++) cin >> A[i];
-
-    int counter = 0;
-    while(true){
-        bool exist_odd = false;
-        for(int i = 0; i < N; i++){
-            if(A[i]%2!=0){
-                exist_odd = true;
-            }
-        }
-        if(exist_odd) break;
-
-        for(int i = 0; i<N; i++){
-            A[i] /= 2;
-        }
-
-        ++counter;
-    }
-
-    cout << counter << endl;
+    cout << "本日は晴天なり" << endl;
 }
